ce5537683ac3542a1067ccfc571390de5da9bda1
Niyush-04 <mauryapradip04@gmail.com>
2025-04-02 15:44:46 +0530

    OS2.0.5.0.VNUINXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,9 +11,9 @@ AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
     vendor \
     odm \
-    vendor_dlkm \
     system \
     product \
+    vendor_dlkm \
     system_ext
 BOARD_USES_RECOVERY_AS_BOOT := true
 
@@ -42,7 +42,7 @@ TARGET_SCREEN_DENSITY := 440
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 bootinfo.fingerprint=breeze_in:12/OS2.0.4.0.VNUINXM:user mtdoops.fingerprint=breeze_in:12/OS2.0.4.0.VNUINXM:user bootconfig
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 bootinfo.fingerprint=breeze_in:12/OS2.0.5.0.VNUINXM:user mtdoops.fingerprint=breeze_in:12/OS2.0.5.0.VNUINXM:user bootconfig
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
@@ -73,9 +73,9 @@ BOARD_SUPER_PARTITION_GROUPS := xiaomi_dynamic_partitions
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     vendor \
     odm \
-    vendor_dlkm \
     system \
     product \
+    vendor_dlkm \
     system_ext
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 
@@ -98,7 +98,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-12-01
+VENDOR_SECURITY_PATCH := 2025-03-01
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
